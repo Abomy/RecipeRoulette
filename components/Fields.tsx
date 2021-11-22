32753168c168833dff90cea64cd016bc58290255
Abomy <abomyy@gmail.com>
2021-11-22 22:46:55 +1300

    Fixed couple style errors, removed .env and db it should no longer be committed

--- components/Fields.tsx
+++ components/Fields.tsx
@@ -13,7 +13,7 @@ interface BaseInputProps
     "onChange" | "onBlur" | "type"
   > {
   label: string;
-  error?: string;
+  error?: { message: string };
 }
 
 export const BaseInput = React.forwardRef<HTMLInputElement, BaseInputProps>(
--- components/styled/button.styled.tsx
+++ components/styled/button.styled.tsx
@@ -1,18 +1,17 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-    border-radius: 50px;
-    border: none;
-    box-shadow: 0 0 10px rgba(0,0,0,0.15);
-    cursor: pointer;
-    font-size: 16px;
-    font-weight: 600;
-    padding: 15px 60px;
-    background-color: ${({ bg }) => bg || "#fff"}
-    color: ${({ color }) => color || "#fff"}
+  border-radius: 50px;
+  border: none;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 600;
+  padding: 15px 60px;
+  background-color: "#fff";
+  color: "#fff";
 
   &:hover {
-    opacity: 0.1;
-    transform: scale( 0.98);
+    transform: scale(0.98);
   }
 `;
